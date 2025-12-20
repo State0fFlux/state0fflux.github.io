ed82f99db6cda8d5e27d0657630ac6a57639693c
State0fFlux <bmanske505@gmail.com>
2025-12-19 18:49:40 -0800

    Updated base URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "./state0fflux.github.io", // Ensures assets load correctly on GitHub Pages
+	base: "./", // Ensures assets load correctly on GitHub Pages
 });
