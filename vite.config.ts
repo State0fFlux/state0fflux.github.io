2d2264e8f44afeaaeeecba8bfdf68f7d6fceaab3
State0fFlux <bmanske505@gmail.com>
2025-12-21 22:32:26 -0800

    Building fr this time

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/", // Ensures assets load correctly on GitHub Pages
+	base: "/", // Ensures assets load correctly on GitHub Pages,
+	build: {
+		outDir: "docs",
+		emptyOutDir: true,
+	},
 });
